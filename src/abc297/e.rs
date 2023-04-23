ec2ab1266d94a73dbda7abd63015b48dd3eed2ea
nusuke <nusukep@gmail.com>
2023-04-24 00:12:08 +0900

    wip

--- src/abc297/e.rs
+++ src/abc297/e.rs
@@ -14,63 +14,102 @@ pub fn main() {
 }
 
 fn answer(input: String) -> String {
-    let (_n, k, a) = {
+    let (_n, k, mut a) = {
         let mut ws = input.split_whitespace();
         let n: i64 = ws.next().unwrap().parse().unwrap();
-        let m: i64 = ws.next().unwrap().parse().unwrap();
+        let m: usize = ws.next().unwrap().parse().unwrap();
         let mut a = ws.map(|s| s.parse().unwrap()).collect::<Vec<i64>>();
-        a.sort();
+        a.sort_by(|a, b| b.cmp(a));
         (n, m, a)
     };
 
-    if a[0] == 1 {
+    if a.last().unwrap() == &1 {
         return k.to_string();
     };
 
-    let a_eles = a.clone().split_off(1);
-    let c = get_amari(a_eles.clone(), a[0] * k);
+    static INF: i64 = 999999999999999999;
+    let mut ans = vec![INF; k];
+    let mut left: usize = 0;
+    let mut right: usize = 0;
+    for i in 0..k {
+        println!(
+            "i={} left={} right={} {}+{}={}",
+            i,
+            left,
+            right,
+            ans[left],
+            ans[right],
+            ans[left] + ans[right]
+        );
 
-    let min = (a[0] * (k - c)).abs();
-    let plus_index = get_amari(a_eles.clone(), min);
-    let min_index = if min / a[0] == 1 {
-        0
-    } else {
-        min / a[0] + plus_index
-    };
+        let last = match a.last() {
+            Some(n) => *n,
+            None => INF,
+        };
+        if ans[left] + ans[right] < last {
+            ans[i] = ans[left] + ans[right];
+            if right < k - 1 {
+                right = right + 1;
+            } else {
+                left += 1;
+                // right = 0;
+            }
+        } else {
+            let poped = match a.pop() {
+                Some(n) => n,
+                None => INF,
+            };
+            ans[i] = poped;
+        }
+    }
+    println!("{:?}", ans);
 
-    let key = if k - min_index - 1 >= 0 {
-        k - min_index - 1
-    } else {
-        0
-    } as usize;
+    return ans[k - 1].to_string();
 
-    println!(
-        "c={} 最小値{} ←index{}  求める値のindexは{}",
-        c, min, min_index, key
-    );
+    // let a_eles = a.clone().split_off(1);
+    // let c = get_amari(a_eles.clone(), a[0] * k);
 
-    let r = (min..=a[0] * k)
-        .into_iter()
-        .filter(|&i| {
-            let r: Vec<&i64> = a
-                .iter()
-                .filter(|&ele| i % ele == 0 || (i - ele) % a[0] == 0)
-                .collect::<Vec<&i64>>();
-            r.len() > 0
-        })
-        .take(key + 1)
-        .collect::<Vec<i64>>();
+    // let min = (a[0] * (k - c)).abs();
+    // let plus_index = get_amari(a_eles.clone(), min);
+    // let min_index = if min / a[0] == 1 {
+    //     0
+    // } else {
+    //     min / a[0] + plus_index
+    // };
 
-    println!("{:?}", r);
-    return r[key].to_string();
+    // let key = if k - min_index - 1 >= 0 {
+    //     k - min_index - 1
+    // } else {
+    //     0
+    // } as usize;
+
+    // println!(
+    //     "c={} 最小値{} ←index{}  求める値のindexは{}",
+    //     c, min, min_index, key
+    // );
+
+    // let r = (min..=a[0] * k)
+    //     .into_iter()
+    //     .filter(|&i| {
+    //         let r: Vec<&i64> = a
+    //             .iter()
+    //             .filter(|&ele| i % ele == 0 || (i - ele) % a[0] == 0)
+    //             .collect::<Vec<&i64>>();
+    //         r.len() > 0
+    //     })
+    //     .take(key + 1)
+    //     .collect::<Vec<i64>>();
+
+    // println!("{:?}", r);
+    // return r[key].to_string();
 }
 
-fn get_amari(array: Vec<i64>, n: i64) -> i64 {
-    return array
-        .iter()
-        .map(|ele| n / ele)
-        .fold(0, |left, right| left + right);
-}
+// fn get_amari(array: Vec<i64>, n: i64) -> i64 {
+//     return array
+//         .iter()
+//         .map(|ele| n / ele)
+//         .fold(0, |left, right| left + right);
+// }
 #[cfg(test)]
 mod tests {
     use crate::abc297::e::answer;
@@ -97,7 +136,7 @@ mod tests {
         ".to_string();
         let res = answer(input);
         // assert
-        assert_eq!(res, "570544381");
+        assert_eq!(res, "5705443819");
     }
 
     #[test]
@@ -115,7 +154,7 @@ a=[3,13,22]
 13| 13,26,39,52 (3を基準に1/4)
 22| 22,44,66 (3を基準に/7)
 
-ans = 3,6,9,12,13,15,18,21,22,24,26,27,30,33,36,39(2),42,44,45....
+ans = 3,6,9,12,13,15,16,18,19,21,22,24,26,27,30,33,36,39(2),42,44,45....
 
 i<a.lengthと置く。
 つまり、a[0]を基準とすると、a[i]はa[i]/a[0]に一回挟まる。
@@ -143,4 +182,20 @@ j= 11では、
 11*a[0]/ a[2] = 1
 
 なので、ansには3(2+1)回a[0]以外の結果が入り込む。
+ */
+
+/*
+4 6
+20 25 30 100
+
+let graph = vec![
+    vec![]
+    vec![20] 0
+    vec![20,25] 0
+    vec![20,25,30] 0
+    vec![20,25,30,40] 1 + 1
+    vec![20,25,30,40.45] 1 + 2
+    vec![20,25,30,40,45,50] 1 + 3
+    vec![20,25,30,40,45,50,55] 2 + 3
+];
  */
